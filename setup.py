6477b032b8139a7ecf540ae1f309c2672762f912
R.W. Ogburn <rwogburn@gmail.com>
2015-11-16 11:52:40 -0800

    RWO: Add MPI stubs

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup, Extension
 import numpy as np
 
 module1 = Extension('pures2hat',
-                    sources = ['pyxpure/pyc_pures2hat.c'],
+                    sources = ['pys2hat/pyc_pures2hat.c'],
                     include_dirs = ['s2hat','pure_s2hat'],
                     library_dirs = ['s2hat','pure_s2hat'],
                     libraries = ['s2hat','s2hat_pure'])
 
-setup (name = 'pyxpure',
+setup (name = 'pys2hat',
        version = '0.1',
-       description = 'Pure-s2hat E/B separation for CMB polarization maps',
+       description = 's2hat with ure-s2hat E/B separation for CMB polarization maps',
        include_dirs = [np.get_include()],
        ext_modules = [module1])
